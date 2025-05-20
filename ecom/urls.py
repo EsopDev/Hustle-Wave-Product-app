88914ddb00ea149a64a93f73a12211f2648b9d6b
emekadefirst <victorchibuogwu33@gmail.com>
2025-05-20 13:16:43 +0100

    auth

--- ecom/urls.py
+++ ecom/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import product_list, create_product, product_detail
+from .views import product_list, create_product, product_detail, login_view, register_view, logout_view
 
 urlpatterns = [
     path('', product_list, name='product_list'),
     path('create/', create_product, name='create_product'),
     path('product/<int:product_id>/', product_detail, name='product_detail'),
+    path('login', login_view, name='login'),
+    path('logout', logout_view, name='logout'),
+    path('signup', register_view, name='register'),
 ]
--- ecom/views.py
+++ ecom/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product, Category
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+
 
 
 def product_list(request):
@@ -25,3 +28,31 @@ def create_product(request):
 
     categories = Category.objects.all()
     return render(request, 'create.html', {'categories': categories})
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("/")
+    return render(request, 'login.html')
+
+def register_view(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        if password1 == password2:
+            user = User.objects.create(username=username, email=email)
+            user.set_password(password1)
+            user.save()
+            return redirect('login')
+    return render(request, 'signup.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
